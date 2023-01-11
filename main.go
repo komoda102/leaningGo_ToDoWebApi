68f985f06b90e61a08ca8831bf901ad90ec2ca21
komoda102 <komoda102@gmail.com>
2023-01-11 19:37:27 +0000

    .

--- main.go
+++ main.go
@@ -10,19 +10,32 @@ import (
 func main() {
 	//http.ListenAndServe(":3000", nil)
 	//
-	t := models.NewTodoItem("Test description")
-	models.AddTodoItem(t)
-	t2 := models.NewTodoItem("Test description 1")
-	models.AddTodoItem(t2)
-	allTodoItems := models.GetTodoItems()
+	t := models.NewTodoItem("Wash your teeth")
+	models.AddTodoItem(*t)
+	t = models.NewTodoItem("Take out the trash")
+	models.AddTodoItem(*t)
+	printTodoList()
 
+	models.ToggleDoneById(1)
+	printTodoList()
+
+	models.ToggleDoneById(1)
+	printTodoList()
+
+	models.RemoveTodoById(0)
+	printTodoList()
+}
+
+func printTodoList() {
+	allTodoItems := models.GetTodoItems()
 	for _, todoItem := range allTodoItems {
 		checkMark := " "
 		if todoItem.Done {
 			checkMark = "x"
 		}
 
-		fmt.Printf("[%v] %v", checkMark, todoItem.Description)
+		fmt.Printf("[%v] %v - %v", checkMark, todoItem.Description, todoItem)
 		fmt.Println()
 	}
+	fmt.Println()
 }
--- models/todo_item.go
+++ models/todo_item.go
@@ -1,5 +1,9 @@
 package models
 
+import (
+	"errors"
+)
+
 type TodoItem struct {
 	ID          int
 	Description string
@@ -7,21 +11,46 @@ type TodoItem struct {
 }
 
 var (
-	todoItems []TodoItem
+	todoItems []*TodoItem
+	nextID    int
 )
 
-func NewTodoItem(description string) TodoItem {
-	return TodoItem{
+func NewTodoItem(description string) *TodoItem {
+	return &TodoItem{
 		Description: description,
 		Done:        false,
 	}
 }
 
 func AddTodoItem(newTodoItem TodoItem) (TodoItem, error) {
-	todoItems = append(todoItems, newTodoItem)
+	newTodoItem.ID = nextID
+	nextID++
+	todoItems = append(todoItems, &newTodoItem)
 	return newTodoItem, nil
 }
 
-func GetTodoItems() []TodoItem {
+func GetTodoItems() []*TodoItem {
 	return todoItems
+}
+
+func ToggleDoneById(id int) error {
+	for _, todoItem := range todoItems {
+		if todoItem.ID == id {
+			todoItem.Done = !todoItem.Done
+			return nil
+		}
+	}
+
+	return errors.New("todo item not found")
+}
+
+func RemoveTodoById(id int) error {
+	for i, todoItem := range todoItems {
+		if todoItem.ID == id {
+			todoItems = append(todoItems[:i], todoItems[i+1:]...)
+			return nil
+		}
+	}
+
+	return errors.New("todo item not found")
 }
